4ed74ba2986cc065da03795452bb619f4ddc20c5
Pedro Jaen <pedro.jaenvilla@gmail.com>
2019-03-19 10:28:48 +0100

    primera version, juego limpio

---
+++ README.md
@@ -0,0 +1,2 @@
+# PlatformNes
+Libgdx Platform Game
---
+++ desktop/src/com/jaen/pedro/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jaen.pedro.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jaen.pedro.PlatformNesGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlatformNesGame(), config);
+	}
+}
